3a2c4fc85f1cf3308e16b279b00f3f0794637713
MansurovSh <147724618+MansurovSh@users.noreply.github.com>
2023-12-25 14:20:03 +0500

    Added the file structure

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string[] arr = (Console.ReadLine().Split(" ")).ToArray();
+Console.WriteLine(string.Join(" ",arr));
+
+string New_matrix(string[] arr ){
+    string new_arr = new string[arr.Length];
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++){
+        if(arr[i].Length <= 3)
+            new_arr[count++] = arr[i];
+    }
+}
